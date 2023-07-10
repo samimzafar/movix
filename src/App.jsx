0a1eabb458fd14862d796a62422228723dd09cd5
sameem.z@dplit.com-at-401940162791 <sameem.z@dplit.com-at-401940162791>
2023-07-10 16:10:47 +0500

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import React, { useEffect } from 'react'
-import { fetchDataFromApi } from './utils/api'
-import { useDispatch, useSelector } from 'react-redux'
-import { getApiConfiguration } from './store/homeSlice'
+import React, { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 const App = () => {
   const dispatch = useDispatch();
-  const { url } = useSelector(state => state.home)
+  const { url } = useSelector((state) => state.home);
   useEffect(() => {
     fetchDataFromApi("movie/popular")
-      .then(res => dispatch(getApiConfiguration(res)))
-      .catch(err => console.log(err))
-  }, [])
-  return (
-    <div className='App'>
-      App
-    </div>
-  )
-}
+      .then((res) => dispatch(getApiConfiguration(res)))
+      .catch((err) => console.log(err));
+  }, []);
+  return <div className="App">App</div>;
+};
 
-export default App
+export default App;
