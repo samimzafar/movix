f621d90978e10f23f2e88accd2e95e98f9d0a048
sameem.z@dplit.com-at-401940162791 <sameem.z@dplit.com-at-401940162791>
2023-07-10 16:14:22 +0500

    prettier

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,9 +4,6 @@ const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 const headers = {
   Authorization: "bearer " + TMDB_TOKEN,
 };
-
-// 80db6c0a250615ea1a75fb7e9c07ba01
-
 export const fetchDataFromApi = async (url, params) => {
   try {
     const { data } = await axios.get(BASE_URL + url, {
