4657091ee70fb1427e6c6e18f7ac5921b8d92976
sameem.z@dplit.com-at-401940162791 <sameem.z@dplit.com-at-401940162791>
2023-07-10 15:50:20 +0500

    new changes redux added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect } from 'react'
 import { fetchDataFromApi } from './utils/api'
+import { useDispatch, useSelector } from 'react-redux'
+import { getApiConfiguration } from './store/homeSlice'
 const App = () => {
-  useEffect(()=>{
-    fetchDataFromApi("movie/popular").then(res=>console.log(res)).catch(err=>console.log(err))
-  },[])
+  const dispatch = useDispatch();
+  const { url } = useSelector(state => state.home)
+  useEffect(() => {
+    fetchDataFromApi("movie/popular")
+      .then(res => dispatch(getApiConfiguration(res)))
+      .catch(err => console.log(err))
+  }, [])
   return (
     <div className='App'>
       App
