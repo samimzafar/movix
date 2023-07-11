cf1b08ac3585b70b0646b356e1224a020405525d
sameem.z@dplit.com-at-401940162791 <sameem.z@dplit.com-at-401940162791>
2023-07-11 11:09:32 +0500

    hero banner

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "./style.scss";
 const HeroBanner = () => {
-  return <h1>HeroBanner</h1>;
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContainer">
+          <span className="title">Welcome</span>
+          <span className="subTitle">
+            Millions of movies,TV shows and people to discover. Explore now.
+          </span>
+          <div className="searchInput">
+            <input type="text" placeholder="search for movie or Tv show ...." />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
